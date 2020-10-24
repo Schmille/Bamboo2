121aaf1b19b41005805aaa6b1ee9686109e0bd6b
Schmille <Schmillemail@gmail.com>
2020-10-24 12:49:35 +0200

    updated to 1.16.3

--- src/main/java/schmille/bamboo2/common/util/BambooUtil.java
+++ src/main/java/schmille/bamboo2/common/util/BambooUtil.java
@@ -5,7 +5,7 @@ import net.minecraft.block.BambooBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,7 +19,7 @@ import java.util.List;
 @Mod.EventBusSubscriber
 public abstract class BambooUtil {
 
-    public static void doSpread(World world, BlockPos pos) {
+    public static void doSpread(IWorld world, BlockPos pos) {
         Iterable<BlockPos> i = BlockPos.getAllInBoxMutable(
                pos.add(-Configuration.BAMBOO_SPREAD.spread_x.get(), -Configuration.BAMBOO_SPREAD.spread_y_bottom.get(), -Configuration.BAMBOO_SPREAD.spread_z.get()),
                pos.add(Configuration.BAMBOO_SPREAD.spread_x.get(), Configuration.BAMBOO_SPREAD.spread_y_top.get(), Configuration.BAMBOO_SPREAD.spread_z.get()));
@@ -36,7 +36,7 @@ public abstract class BambooUtil {
                 BlockState down = world.getBlockState(bp.down(1));
 
                 if(ConfigBlockUtil.canSpreadTo(down.getBlock())) {
-                    world.setBlockState(bp, Blocks.BAMBOO_SAPLING.getDefaultState());
+                    world.setBlockState(bp, Blocks.BAMBOO_SAPLING.getDefaultState(), (1|2));
                     Bamboo2.getLogger().debug(String.format("doSpread: Placed bamboo sapling at %d %d %d", bp.getX(), bp.getY(), bp.getZ()));
                     break;
                 }
@@ -44,7 +44,7 @@ public abstract class BambooUtil {
         }
     }
 
-    public static BlockPos findRoot(World world, BlockPos pos) {
+    public static BlockPos findRoot(IWorld world, BlockPos pos) {
         BlockPos last = pos;
 
         for(int i = 0; i < 255; i++) {
@@ -65,20 +65,20 @@ public abstract class BambooUtil {
         if(!Configuration.BAMBOO_SPREAD.do_bamboo_spread.get())
             return;
 
-        if(event.getWorld().getWorld().isRemote)
+        if(event.getWorld().isRemote())
             return;
 
         if(!(event.getState().getBlock() instanceof BambooBlock))
             return;
 
-        if(Configuration.BAMBOO_SPREAD.only_spread_grown.get() && ((BambooBlock) event.getState().getBlock()).canGrow(event.getWorld().getWorld(), event.getPos(), event.getState(), event.getWorld().getWorld().isRemote))
+        if(Configuration.BAMBOO_SPREAD.only_spread_grown.get() && ((BambooBlock) event.getState().getBlock()).canGrow(event.getWorld(), event.getPos(), event.getState(), event.getWorld().isRemote()))
             return;
 
             double chance = NumberUtil.randomChanceFromPercentile(Configuration.BAMBOO_SPREAD.spread_chance.get(), event.getWorld().getRandom());
             if(chance <= Configuration.BAMBOO_SPREAD.spread_chance.get()) {
                 Bamboo2.getLogger().debug(String.format("onCropGrowth: Bamboo spread triggered with value (%f <= %f) on Block %d %d %d",
                         chance, Configuration.BAMBOO_SPREAD.spread_chance.get(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ()));
-                doSpread(event.getWorld().getWorld(), findRoot(event.getWorld().getWorld(), event.getPos()));
+                doSpread(event.getWorld(), findRoot(event.getWorld(), event.getPos()));
         }
     }
 
